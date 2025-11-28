5c1b7b4644d158aed8e1d491448ce0825c1a8f8a
ragoon99 <psychodragoon99@gmail.com>
2025-11-28 21:21:43 +0545

    feat: tmux config

--- scripts/tmux_session.sh
+++ scripts/tmux_session.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 SESSION_NAME="ghostty"
-TMUX_PATH=/opt/homebrew/bin//tmux
+TMUX_PATH=which tmux
 
 # Check if the session already exists
 $TMUX_PATH has-session -t $SESSION_NAME 2>/dev/null
 
 if [ $? -eq 0 ]; then
- # If the session exists, reattach to it
- $TMUX_PATH attach-session -t $SESSION_NAME
+	# If the session exists, reattach to it
+	$TMUX_PATH attach-session -t $SESSION_NAME
 else
- # If the session doesn't exist, start a new one
- $TMUX_PATH new-session -s $SESSION_NAME -d
- $TMUX_PATH attach-session -t $SESSION_NAME
+	# If the session doesn't exist, start a new one
+	$TMUX_PATH new-session -s $SESSION_NAME -d
+	$TMUX_PATH attach-session -t $SESSION_NAME
 fi
