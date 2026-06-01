7dc238db2a10e9af8529bf7f0da69bc974e54d94
ragoon99 <psychodragoon99@gmail.com>
2026-06-02 03:37:01 +0545

    feat(ashell): added ashell config

--- hyprland/keybinds.lua
+++ hyprland/keybinds.lua
@@ -61,3 +61,5 @@ hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+
+hl.bind(mainMod .. " + b", hl.dsp.exec_cmd("pkill -SIGUSR1 -f ashell"))
